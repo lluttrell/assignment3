08e9c6b36f5cdf4e3b18112ea9b39e4fb68aa4eb
Richard Williams <richard.lluttrell@gmail.com>
2020-11-12 10:25:57 -0700

    Added some more functionality

--- app.js
+++ app.js
@@ -1,13 +1,44 @@
+let socket = io();
+
+if (localStorage.getItem('username')) {
+  socket.emit('chat message', `/name ${localStorage.getItem('username')}`);
+}
+
+if (localStorage.getItem('color')) {
+  socket.emit('chat message', `/color ${localStorage.getItem('color')}`);
+}
+
+function appendChatMessage(socket, message) {
+  let msg = JSON.parse(message)
+
+  console.log(msg)
+  $('#messages').append($('<li>').text(`${msg.timestamp} ${msg.user.name}: ${msg.content}`));
+}
+
 $(function () {
-  var socket = io();
+
   $('form').submit(function(e) {
     e.preventDefault();
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
     return false;
   });
-  
-  socket.on('chat message', function(msg) {
-    $('#messages').append($('<li>').text(msg));
+
+  socket.on('message history', function(msgHist) {
+    let messages = JSON.parse(msgHist);
+    messages.forEach(function(message) {
+      appendChatMessage(null,message)
+    })
+  })
+
+  socket.on('chat message', appendChatMessage.bind(null,socket));
+
+  socket.on('user list', function(userList) {
+    let users = JSON.parse(userList);
+    $('#users').empty();
+    users.forEach(function(user) {
+      $('#users').append($('<li>').text(`${user.name}`));
+    })
   })
 });
+
--- server.js
+++ server.js
@@ -10,29 +10,93 @@ class User {
     this.name = createRandomName();
     this.color = '000000';
   }
+
+  setName(name) {
+    if (usernameExists(name)) {
+      return false;
+    }
+    this.name = name;
+    return true;
+  }
+
+  setColor(color) {
+    if (/^#([0-9A-F]{3}){1,2}$/i.test(color)) {
+      this.color = color;
+      return true;
+    }
+    return false;
+  }
 }
 
 class Message {
   constructor(user, content) {
     this.user = user;
     this.content = content
-    this.timestamp = Date.now();
+    this.timestamp = this.getTimestamp();
+  }
+
+  getTimestamp() {
+    let timestamp = new Date();
+    return `${timestamp.getHours()}:${timestamp.getMinutes()}:${timestamp.getSeconds()}`
+  }
+
+  isColorChange() {
+    return this.content.startsWith('/color ');
+  }
+  
+  isNameChange() {
+    return this.content.startsWith('/name ');
+  }
+
+  toColorString() {
+    if (this.isColorChange()) {
+      return this.content.replace('/color ', '');
+    } else {
+      return false
+    }
+  }
+
+  toNameString() {
+    if (this.isNameChange()) {
+      return this.content.replace('/name ', '');
+    } else {
+      return false
+    }
   }
 }
 
+
+/**
+ * Checks if name is already taken in the user list
+ * @param {string} uname Username to check
+ */
+function usernameExists(uname) {
+  return users.filter(user => user.name === uname).length > 0
+}
+
+/**
+ * Returns a random integer between min and max
+ * @param {int} min Minimum value of random number
+ * @param {int} max Maximum value of random number
+ */
 function getRandomInt(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min) + min);
 }
 
+/**
+ * Creates a random username, checks to ensure name isn't taken
+ */
 function createRandomName() {
-  let uname = `user${getRandomInt(100,999)}`
-  while (users.filter(user => user.name === uname).length > 0 ) {
-    uname = `user${getRandomInt(100,999)}`
-  }
+  let uname
+  do uname =`user${getRandomInt(100,999)}`
+  while (usernameExists(uname))
   return uname
 }
+
+
+
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.html');
@@ -49,21 +113,31 @@ app.get('/app.js', (req,res) => {
 io.on('connection', (socket) => {
   let user = new User();
   users.push(user);
-  io.emit('chat message', `${user.name} connected`);
-  console.log(`${user.name} connected`)
+  io.emit('user list', JSON.stringify(users))
+  //io.emit('chat message', `${user.name} connected`);
+
+  socket.emit('message history', JSON.stringify(messages))
 
   socket.on('disconnect', () => {
     users = users.filter(u => u.name != user.name )
     io.emit('chat message', `${user.name} disconnected`);
-    console.log(`${user.name} disconnected`)
   });
 
   socket.on('chat message', (msg) => {
-    io.emit('chat message', `${user.name}: ${msg}`);
-    console.log('chat message', `${user.name}: ${msg}`);
+    let message = new Message(user, msg);
+    if (message.isColorChange()) {
+      user.setColor(message.toColorString())
+    } else if (message.isNameChange()) {
+      user.setName(message.toNameString())
+    } else {
+      if (messages.length >= 200) messages = messages.shift();
+      messages.push(message);
+      io.emit('chat message', JSON.stringify(message));
+    }
   });
 })
 
-http.listen(3000, () => {
-  console.log('listening on *:3000');
+const port = process.env.PORT || 41399
+http.listen(port, () => {
+  console.log(`listening on *:${port}`);
 })
